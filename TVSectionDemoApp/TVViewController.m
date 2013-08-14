6b28d9f8ee3aad4255c8d1779f2718571afc15c1
Max Lunin <nut.code.monkey@gmail.com>
2013-08-14 22:44:56 +0300

    update README

--- TVSectionDemoApp/TVViewController.m
+++ TVSectionDemoApp/TVViewController.m
@@ -21,27 +21,27 @@
     __weak UITableView* tableView = self.tableView;
     
     TVSection* numberSection = [[TVSection sectionWithItems:@[ @1, @2, @3, @4, @5 ]
-                                             cellGenerator:^(TVSection* section, NSNumber* number, NSUInteger idx)
+                                              cellGenerator:^(TVSection* section, NSNumber* number, NSUInteger idx)
                                  {
                                      UITableViewCell* cellForNumber = [tableView dequeueReusableCellWithIdentifier:@"cellForNumber"];
                                      cellForNumber.textLabel.text = [number stringValue];
                                      return cellForNumber;
                                  }]
-                                onClick:^(TVSection *section, id item, NSUInteger index)
-                                 {
-                                     NSLog(@"you select first section with number: %@", item);
-                                 }];
+                                onClick:^(TVSection *section, NSNumber* number, NSUInteger index)
+                                {
+                                    NSLog(@"you select first section with number: %@", number);
+                                }];
     
     TVSection* stringSection = [[TVSection sectionWithItems:@[ @"string 1", @"string 2", @"string 3"]
-                                             cellGenerator:^(TVSection* section, NSString* string, NSUInteger idx)
+                                              cellGenerator:^(TVSection* section, NSString* string, NSUInteger idx)
                                  {
                                      UITableViewCell* cellForString = [tableView dequeueReusableCellWithIdentifier:@"cellForStrings"];
                                      cellForString.textLabel.text = string;
                                      return cellForString;
                                  }]
-                                onClick:^(TVSection *section, id item, NSUInteger index)
+                                onClick:^(TVSection *section, NSString* string, NSUInteger index)
                                 {
-                                    NSLog(@"you select second section with string: %@", item);
+                                    NSLog(@"you select second section with string: %@", string);
                                 }];
     
     self.sections = @[numberSection, stringSection];
